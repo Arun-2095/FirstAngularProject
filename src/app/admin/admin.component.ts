1c208fa7223d2e867f29e7482644afb0cfbcd76a
Arun-2095 <arunagiri2195@gamil.com>
2019-07-11 18:13:11 +0530

    admin panel created

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+   username: string ;
+   password: string ;
   constructor() { }
 
   ngOnInit() {
   }
 
+onSubmit(){
+  console.log(this.username);
+}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
-
+import { HomeContentComponent } from './home-content/home-content.component';
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeContentComponent
+  },
   {
     path: 'admin' ,
     component : AdminComponent
